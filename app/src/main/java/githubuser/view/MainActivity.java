768354238b6b1f985233fc2f1560dfaa9f15bf7d
Jakana Kiwanuka <k.jak86@yahoo.com>
2018-05-30 10:49:12 +0300

    [Chore 156185411] Update Readme

--- app/src/main/java/githubuser/view/MainActivity.java
+++ app/src/main/java/githubuser/view/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ProgressBar;
 
 import com.example.jakanakiwanuka.mrmlevelup.R;
 import java.util.ArrayList;
